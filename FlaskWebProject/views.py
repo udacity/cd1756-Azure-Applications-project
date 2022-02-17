5205f9777fd70051158a7b829c118b8e44bf8b57
Ibrahim Cetinkaya <Ibrahimcetinkaya@live.com.au>
2022-02-17 13:28:40 +1100

    added build auth url

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -126,5 +126,7 @@ def _build_msal_app(cache=None, authority=None):
 
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
