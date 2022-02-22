178ba68460c83f729c5d36b4c1e9ab538aad3393
Ibrahim Cetinkaya <Ibrahimcetinkaya@live.com.au>
2022-02-22 11:41:47 +1100

    logging for login/unsuccessful login

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -79,12 +79,14 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning('Invalid username or password')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+            app.logger.warning('Successful User Login')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
