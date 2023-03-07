0efba3131eea7d1c1291cfc55b646bc99c003fd7
tuyennguyen99 <nguyentuyen95.bt@gmail.com>
2023-03-08 05:10:37 +0700

    Remove todo

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,7 +11,6 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 app.logger.setLevel(logging.WARNING)
-# TODO: Add any logging levels and handlers with app.logger
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 
