d8e2eb1478fce2fd72b9ced7cfef24dbb680c7db
tuyennguyen99 <nguyentuyen95.bt@gmail.com>
2023-03-08 05:10:45 +0700

    Remove todo

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -112,7 +112,6 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             code=request.args['code'],
             scopes=Config.SCOPE,
@@ -123,7 +122,6 @@ def authorized():
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
-        # TODO: Auto create user if user doesn't exist in the system
         # user = User.query.filter_by(username="admin").first()
         username = _get_username(session=session)
         user = _create_user(username=username)
@@ -153,15 +151,12 @@ def _get_username(session):
 def _create_user(username):
     user = User.query.filter_by(username=username).first()
     if user == None:
-        # TODO: Insert user into db
         newUser = User()
         newUser.save_changes(username=username)
         return newUser
-    # TODO: Return user
     return user
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
     cache = msal.SerializableTokenCache()
     if session.get(tokenCache):
         cache.deserialize(session[tokenCache])
@@ -170,16 +165,13 @@ def _load_cache():
 def _save_cache(cache):
     if cache.has_state_changed:
         session[tokenCache] = cache.serialize()
-    # TODO: Save the cache, if it has changed
     pass
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
     return msal.ConfidentialClientApplication(client_id=Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
                                               client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
     return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes=scopes or [],
         state=state or str(uuid.uuid4()),
