2c36f1ea6427b89da837864acffb4505f5114857
JasonMartino <jason.martino@chevron.com>
2024-10-04 12:41:52 +0800

    Update forms.py

--- FlaskWebProject/forms.py
+++ FlaskWebProject/forms.py
@@ -2,7 +2,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from wtforms.widgets import TextArea
 
 class LoginForm(FlaskForm):
@@ -13,7 +13,9 @@ class LoginForm(FlaskForm):
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    sub_title = StringField('Sub Title')
     author = StringField('Author', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
     image_path = FileField('Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    remove_image_path = BooleanField('Remove Image')
     submit = SubmitField('Save')
