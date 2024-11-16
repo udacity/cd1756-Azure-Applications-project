de5ca4ec19f7eb170fbaf4b78874fc60eb319fbc
cameron-hlxh <cameron.barrett@chevron.com>
2024-11-16 00:02:43 +0000

    update veiws to log success correctly

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,8 +69,8 @@ def login():
             flash('Invalid username or password')
             app.logger.warning("Invalid login attempt")
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
         app.logger.info(f"{user} logged in successfully")
+        login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
