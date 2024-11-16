39156966a299a3a9c64e2ea0abcca129e8813af5
cameron-hlxh <cameron.barrett@chevron.com>
2024-11-16 02:06:28 +0000

    updating views logger

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,13 +67,13 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            app.logger.warning("Invalid login attempt")
+            app.logger.info("Invalid login attempt")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info(f"{user.username} logged in successfully")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-        app.logger.info("meeeeee logged in successfully")
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
