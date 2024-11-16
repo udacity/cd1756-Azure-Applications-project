95d87e9f1df8bf0138507792098aecf20bc6ceb7
cameron-hlxh <cameron.barrett@chevron.com>
2024-11-16 00:11:54 +0000

    chanegd user to user.username in views

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,7 +69,7 @@ def login():
             flash('Invalid username or password')
             app.logger.warning("Invalid login attempt")
             return redirect(url_for('login'))
-        app.logger.info(f"{user} logged in successfully")
+        app.logger.info(f"{user.username} logged in successfully")
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
